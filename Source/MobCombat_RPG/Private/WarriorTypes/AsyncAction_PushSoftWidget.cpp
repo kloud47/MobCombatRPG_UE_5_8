9141cdcca54bede0a7d8695cf92704b4f3ad5098
kloud47 <136368351+kloud47@users.noreply.github.com>
2026-07-06 03:38:04 +0530

    ok

--- Source/MobCombat_RPG/Private/WarriorTypes/AsyncAction_PushSoftWidget.cpp
+++ Source/MobCombat_RPG/Private/WarriorTypes/AsyncAction_PushSoftWidget.cpp
@@ -4,10 +4,11 @@
 #include "WarriorTypes/AsyncAction_PushSoftWidget.h"
 
 #include "Engine/Engine.h"
+#include "Subsystems/FrontendUISubsystem.h"
 
 UAsyncAction_PushSoftWidget* UAsyncAction_PushSoftWidget::PushSoftWidget(UObject* WorldContextObject,
-                 APlayerController* OwningPlayerController, TSoftClassPtr<UWidget_ActivatableWidget> InSoftWidgetClass,
-                 UPARAM(meta = (Categories = "UI.WidgetStack")) FGameplayTag InWidgetStackTag, bool bFocusOnNewlyPushedWidget)
+                                                                         APlayerController* OwningPlayerController, TSoftClassPtr<UWidget_ActivatableWidget> InSoftWidgetClass,
+                                                                         UPARAM(meta = (Categories = "UI.WidgetStack")) FGameplayTag InWidgetStackTag, bool bFocusOnNewlyPushedWidget)
 {
 	checkf(!InSoftWidgetClass.IsNull(),TEXT("PushSoftWidgetToStack was passed a null soft widget class "));
 
@@ -28,5 +29,5 @@ UAsyncAction_PushSoftWidget* UAsyncAction_PushSoftWidget::PushSoftWidget(UObject
 
 void UAsyncAction_PushSoftWidget::Activate()
 {
-	Super::Activate();
+	UFrontendUISubsystem* FrontEndUISubsystem = UFrontendUISubsystem::Get(CachedOwningWorld.Get());
 }
