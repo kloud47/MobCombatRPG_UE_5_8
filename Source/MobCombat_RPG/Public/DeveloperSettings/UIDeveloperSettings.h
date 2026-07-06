c8b90c57d76425b48b1f9afd68344c8a5c40c58e
kloud47 <136368351+kloud47@users.noreply.github.com>
2026-07-06 22:42:52 +0530

    Developer Settings for UI

---
+++ Source/MobCombat_RPG/Public/DeveloperSettings/UIDeveloperSettings.h
@@ -0,0 +1,24 @@
+// Priyanshu Shukla All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DeveloperSettings.h"
+#include "UObject/SoftObjectPtr.h"
+#include "UIDeveloperSettings.generated.h"
+
+class UWidget_ActivatableWidget;
+/**
+ *
+ */
+UCLASS(Config = Game, defaultconfig, meta = (DisplayName = "MobCombat UI Settings"))
+class MOBCOMBAT_RPG_API UUIDeveloperSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(Config, EditAnywhere, Category = "Widget Reference", meta = (ForceInlineRow, Categories = "Frontend.Widget"))
+	TMap<FGameplayTag, TSoftClassPtr<UWidget_ActivatableWidget>> FrontendWidgetMap;
+	
+};
